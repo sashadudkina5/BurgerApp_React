6e14082cd4a7d964c3c3fd5c9f4317af8c6da865
sashadudkina5 <uu.aaa@mail.ru>
2023-11-04 13:48:12 +0300

    fixed ingredient detail modal

--- src/redux_services/selectors.js
+++ src/redux_services/selectors.js
@@ -3,3 +3,4 @@ export const getListOfIngredients = (state) => state.ingredientsStore;
 export const getIngredientDetails = (state) => state.ingredientDetailsStore.ingredientDetails;
 export const getBurgerIngredients = (state) => state.ingredientsStore.ingredientsData.data;
 export const getBunData = (state) => state.constructorStore.bun;
+export const getIngredientDetailsModalState = (state) => state.ingredientDetailsStore.isIngredientDetailModalOpen;
